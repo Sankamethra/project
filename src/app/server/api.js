59fb91e0c0d9995122ef5ebf3fe42f4e2e4f9c6f
Sankamethra <sankamethrasenthilkumar@gmail.com>
2022-11-10 21:19:27 +0530

    first commit

--- src/app/server/api.js
+++ src/app/server/api.js
@@ -57,48 +57,49 @@ router.post('/login',(req,res)=>{
 
 router.post('/file',(req,res)=>{
     let data=req.body
-    mongo.connect(url,(err,database)=>{
-        var x=database.db("mydb")
-        c=x.collection("new").countDocuments()
-    })
-    if(c==null){
-    d.collection("new").createIndex({"sno":1},{unique:true})  
-
-    for(let i=0;i<data.length;i++){ 
-        const dat={
-            sno:data[i]['sno'],
-            drawingnumber:data[i]['drawingnumber'],
-            componentname:data[i]['componentname'],
-            partname:data[i]['partname'],
-            material:data[i]['material'],
-            sequencename:data[i]['sequencename'],
-            opn:data[i]['opn'],
-            bar:data[i]['bar'],
-            insertspec:data[i]['insertspec'],
-            noofedge:data[i]['noofedge'],
-            edgelife:data[i]['edgelife'],
-            make:data[i]['make'],
-            supplier:data[i]['supplier'],
-            rate:data[i]['rate'],
-            insertlife:data[i]['insertlife'],
-            alternateinsert:data[i]['alternateinsert'],
-            noofedgeforalternative:data[i]['noofedgeforalternative']
+    async function run(){
+        let x=d.collection("new")
+        let y=await x.estimatedDocumentCount()
+        if(y==0){
+            d.collection("new").createIndex({"sno":1},{unique:true})  
         
-
+            for(let i=0;i<data.length;i++){ 
+                const dat={
+                    sno:data[i]['sno'],
+                    drawingnumber:data[i]['drawingnumber'],
+                    componentname:data[i]['componentname'],
+                    partname:data[i]['partname'],
+                    material:data[i]['material'],
+                    sequencename:data[i]['sequencename'],
+                    opn:data[i]['opn'],
+                    bar:data[i]['bar'],
+                    insertspec:data[i]['insertspec'],
+                    noofedge:data[i]['noofedge'],
+                    edgelife:data[i]['edgelife'],
+                    make:data[i]['make'],
+                    supplier:data[i]['supplier'],
+                    rate:data[i]['rate'],
+                    insertlife:data[i]['insertlife'],
+                    alternateinsert:data[i]['alternateinsert'],
+                    noofedgeforalternative:data[i]['noofedgeforalternative']
+                
+        
+                    
+                }
+        
+                d.collection("new").insertMany([dat],{upsert:true})
+            }
+            res.send(JSON.stringify("data loaded"))
             
         }
-
-        d.collection("new").insertMany([dat],{upsert:true})
-    }
-    res.send(JSON.stringify("data loaded"))
-    
-}
-
-else{
+        
+        else{
             res.send(JSON.stringify("Data exist"))
+        
+        }
 
-}
-
+    }
+    run()
     
 })
 
@@ -212,51 +213,48 @@ router.post('/getupdate',(req,res)=>{
 
 router.post('/grid',(req,res)=>{
     let data=req.body
-
-
-    for(let i=0;i<data.length;i++){
-        let count=d.collection("new").count()
-        const dat={
-            sno:count+1,
-            drawingnumber:data[i]['drawingnumbernew'],
-            componentname:data[i]['componentnamenew'],
-            partname:data[i]['partnamenew'],
-            material:data[i]['materialnew'],
-            sequencename:data[i]['sequencenamenew'],
-            opn:data[i]['opnnew'],
-            bar:data[i]['barnew'],
-            insertspec:data[i]['insertspecnew'],
-            noofedge:data[i]['noofedgenew'],
-            edgelife:data[i]['edgelifenew'],
-            make:data[i]['makenew'],
-            supplier:data[i]['suppliernew'],
-            rate:data[i]['ratenew'],
-            insertlife:data[i]['insertlifenew'],
-            alternateinsert:data[i]['alternateinsertnew'],
-            noofedgeforalternative:data[i]['noofedgeforalternativenew']
+    async function run(){
+    let x=d.collection("new")
+    let count=await x.estimatedDocumentCount()
+    
+    if(data){
+        for(let i=0;i<data.length;i++){
+            
+            const dat={
+                sno:count+1,
+                drawingnumber:data[i]['drawingnumbernew'],
+                componentname:data[i]['componentnamenew'],
+                partname:data[i]['partnamenew'],
+                material:data[i]['materialnew'],
+                sequencename:data[i]['sequencenamenew'],
+                opn:data[i]['opnnew'],
+                bar:data[i]['barnew'],
+                insertspec:data[i]['insertspecnew'],
+                noofedge:data[i]['noofedgenew'],
+                edgelife:data[i]['edgelifenew'],
+                make:data[i]['makenew'],
+                supplier:data[i]['suppliernew'],
+                rate:data[i]['ratenew'],
+                insertlife:data[i]['insertlifenew'],
+                alternateinsert:data[i]['alternateinsertnew'],
+                noofedgeforalternative:data[i]['noofedgeforalternativenew']
+            }
+    
+            d.collection("new").insert([dat],{upsert:true})
         }
-
-        d.collection("new").insertMany([dat],{upsert:true})
+    
+        res.send(JSON.stringify("data inserted"))
+    
     }
-
-    res.send(JSON.stringify("data inserted"))
-
-
-
-
-
-        
+    else{
+        res.send("Input not found")
+    }
     
+
+    }
+    run()
     
-    // res.send(JSON.stringify(drawingnumber))
-    // d.collection('data').insertOne(obj,(err)=>{
-    //     if(err) throw err
-    //     console.log("data inserted")
-    //     res.send(JSON.stringify("data inserted"))
-    // })
 })
-
-
 
 
 module.exports=router
